32d230835e326033f7016712d279d288b7ca2990
Rodrigo Flores <rodrigoraulflores@gmail.com>
2021-11-02 21:27:26 -0600

    Documentation regarding modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms'
+// Here I added a form library with the structure shown below
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule  // this declaration is also important
   ],
   providers: [],
   bootstrap: [AppComponent]
